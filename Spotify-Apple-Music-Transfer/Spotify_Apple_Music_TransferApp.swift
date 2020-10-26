7a3c95fca9335fd326f0e3238813e47dd4d27a9f
Ted <teddsbennett@gmail.com>
2020-10-26 16:34:59 +0000

    Initial Commit

---
+++ Spotify-Apple-Music-Transfer/Spotify_Apple_Music_TransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_Apple_Music_TransferApp.swift
+//  Spotify-Apple-Music-Transfer
+//
+//  Created by Ted Bennett on 26/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_Apple_Music_TransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
