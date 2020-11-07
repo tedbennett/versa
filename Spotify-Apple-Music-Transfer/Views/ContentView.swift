baa8b130e7fae03042570a6b32fbaae7b19f6ab8
Ted <teddsbennett@gmail.com>
2020-11-07 15:14:04 +0000

    Remove add services view

--- Spotify-Apple-Music-Transfer/Views/ContentView.swift
+++ Spotify-Apple-Music-Transfer/Views/ContentView.swift
@@ -9,40 +9,24 @@ import SwiftUI
 import SpotifyAPI
 
 struct ContentView: View {
-    
-    @ObservedObject var AppleMusicAuth = AppleMusicAuthViewModel.shared
-    @ObservedObject var SpotifyAuth = SpotifyAuthViewModel.shared
-    
+
     @ObservedObject var clipboardViewModel = OpenLinkViewModel.shared
-    
-    init() {
-        SpotifyAuth.initialize()
-    }
     
     var body: some View {
         TabView {
-            if AppleMusicAuth.authenticated {
-                AppleMusicPlaylistList().tabItem {
-                    Text("Apple")
-                    Image(uiImage: UIImage(named: "apple")!)
-                }
+            AppleMusicView().tabItem {
+                Text("Apple")
+                Image(uiImage: UIImage(named: "apple")!)
             }
-            if SpotifyAuth.authenticated {
-                SpotifyPlaylistList().tabItem {
-                    Text("Spotify")
-                    Image(uiImage: UIImage(named: "spotify")!)
-                }
-            }
-            if !(SpotifyAuth.authenticated && AppleMusicAuth.authenticated) {
-                AddServiceView().tabItem {
-                    Text("Add Service")
-                    Image(systemName: "plus")
-                }
+
+            SpotifyView().tabItem {
+                Text("Spotify")
+                Image(uiImage: UIImage(named: "spotify")!)
             }
         }
         .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
             clipboardViewModel.parseClipboard(UIPasteboard.general.string)
-            }
+        }
         
         .sheet(isPresented: $clipboardViewModel.foundLink) {
             OpenLinkView()
